085967fbba965332dbd293db8e79d47142f450ca
Zack <zack@fbts.ca>
2018-11-02 13:05:00 -0600

    Update crackle.js

--- dist/crackle.js
+++ dist/crackle.js
@@ -1,5 +1,37 @@
 var crackle = function(tag) {
   var object = NULL
+  var methods = function() {}
+methods.prototype = {
+  all: function(object) {
+    l = {}
+    if (this.type == "class" || this.type == "tag") {
+      Object.keys(object[0].style).forEach(function(key) {
+        l[key] = function(val) {
+          object.forEach(function(obj) {
+            obj.style[key] = val;
+          }
+        }
+      })
+    } else if (this.type == "id") {
+      Object.keys(object.style).forEach(function(key) {
+        l[key] = function(val) {
+          object.style[key] = val;
+        }
+      }
+    }
+                                        // Custom Methods
+    l.text = function(text) {
+      if (type == "class" || type == "tag") {
+        Object.keys(object).forEach(function(elem) {
+          elem.innerHTML = text
+        }
+      } else if (type == "id") {
+          object.innerHTML = text
+      }
+    }
+    return l
+  }
+}
   if (tag[0] == "#") {
     object = document.getElementById(tag.drop(1))
     methods.type = "id"
